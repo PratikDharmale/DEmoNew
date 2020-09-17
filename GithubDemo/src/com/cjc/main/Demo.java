dd3f9fe53d4543b8ccba3cc8957abe145f35891d
Shree <Shree@DESKTOP-P5VCUL6>
2020-09-17 23:28:36 +0530

    Change by pratik1

--- GithubDemo/src/com/cjc/main/Demo.java
+++ GithubDemo/src/com/cjc/main/Demo.java
@@ -1,10 +1,17 @@
 package com.cjc.main;
 
 public class Demo {
+	
+	public void sum()
+	{
+		System.out.println("sum");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 1");
+		
+		
 	}
 
 }
